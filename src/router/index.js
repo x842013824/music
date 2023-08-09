b6b18c9df0fe4751506b69974ef653f7cbe17d18
x842013824 <44201099+x842013824@users.noreply.github.com>
2023-08-10 00:39:38 +0800

    路由部分修改
    * 分离主体内容部分至HomeView
    * 新增About界面和Manage界面
    * 增加了导航的link和激活样式
    * 增加了catchAll导航至主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ManageView from '@/views/ManageView.vue'
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: HomeView
+  },
+  {
+    name: 'about',
+    path: '/about',
+    component: AboutView
+  },
+  {
+    name: 'manage',
+    alias: '/manage',
+    path: '/manage-music',
+    component: ManageView
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' }
+  }
+]
+
+/**
+ * * 使用linkExactAtiveClass来自定义精准匹配路由时的router-link样式
+ */
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
